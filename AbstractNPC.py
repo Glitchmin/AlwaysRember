bf25fe92201d67cdf319b61d620700a454a0e481
Adam <adam.p.kulczycki@gmail.com>
2022-07-01 22:15:10 +0200

    AbstractNpc: added Texture

--- AbstractNPC.py
+++ AbstractNPC.py
@@ -1,15 +1,21 @@
+from pygame.surface import Surface
+
 from DirectionsEnum import Direction
 
 
 class AbstractNPC:
-    def __init__(self, hp, x, y):
+    def __init__(self, hp: int, x: int, y: int, texture: Surface):
         self.__hp = hp
         self.__x = x
         self.__y = y
+        self.__texture = texture
 
     def move(self, dir: Direction):
         self.__x -= dir.value[0]
         self.__y -= dir.value[1]
 
-    def getPosition(self):
-        return (self.__x, self.__y)
+    def getPosition(self) -> (int, int):
+        return self.__x, self.__y
+
+    def getTexture(self) -> Surface:
+        return self.__texture
