210a944922900661a44a4eebb260b517d6c7600b
Adam <adam.p.kulczycki@gmail.com>
2022-07-01 22:22:00 +0200

    Added Tile

---
+++ Tile.py
@@ -0,0 +1,17 @@
+from enum import Enum
+
+from AbstractNPC import AbstractNPC
+
+walkable = [False, True]
+
+
+class TileType(Enum):
+    GRASS = 0
+    BUILDING = 1
+
+
+class Tile:
+    def __init__(self, tileType: TileType):
+        self.__tileType = tileType
+        self.__npcsTab: [AbstractNPC] = []
+        self.item = None
