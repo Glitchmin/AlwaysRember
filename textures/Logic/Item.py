5899007e03f106487a5e23978c05f0b543783a6e
Adam <adam.p.kulczycki@gmail.com>
2022-07-01 22:42:55 +0200

    Items added

---
+++ textures/Logic/Item.py
@@ -0,0 +1,28 @@
+from enum import Enum
+
+
+class AbstractItem:
+    def __init__(self, name):
+        self.name = name
+
+
+class Weapon(AbstractItem):
+    def __init__(self, name, dmg, cd):
+        super().__init__(name)
+        self.dmg = dmg
+        self.cd = cd
+
+
+class LightSource(AbstractItem):
+    def __init__(self, name, radius, angle):
+        super().__init__(name)
+        self.radius = radius
+        self.angle = angle
+
+
+ItemTab = [Weapon("Crowbar", 1, 0.5), LightSource("torch", 2, 360), ]
+
+
+class Item(Enum):
+    CROWBAR = 0
+    TORCH = 1
