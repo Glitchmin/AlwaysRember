bf5bdcaccf58a6947675c2b0c5e94030eb476106
Bartek Pacia <barpac02@gmail.com>
2022-07-01 22:16:45 +0200

    add very simple rendering of ground

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import pygame
 
+SCREEN_WIDTH = 720
+SCREEN_HEIGHT = 480
+
 pygame.init()
+pygame.display.set_caption("AlwaysRember")
 
 screen = pygame.display.set_mode([500, 500])
 
 running = True
+
+ground_texture_file = open("textures/ground.png")
+ground_texture = pygame.image.load(ground_texture_file)
+
+print(f'width: {ground_texture.get_width()}, height: {ground_texture.get_height()}')
 
 while running:
     for event in pygame.event.get():
@@ -14,6 +23,10 @@ while running:
     screen.fill((255, 255, 255))
 
     pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
+
+    for i in range(SCREEN_HEIGHT // 16):
+        for j in range(SCREEN_WIDTH // 16):
+            screen.blit(ground_texture, (ground_texture.get_width() * i, ground_texture.get_height() * j,))
 
     pygame.display.flip()
 
